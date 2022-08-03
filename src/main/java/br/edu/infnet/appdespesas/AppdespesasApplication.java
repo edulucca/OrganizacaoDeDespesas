8a97d3a33b5217afbe13a11011ec1cfc28d5f4ad
Eduardo de Lucca <eduardo.luccati@gmail.com>
2022-08-03 19:21:24 -0300

    Initial Commit

---
+++ src/main/java/br/edu/infnet/appdespesas/AppdespesasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appdespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdespesasApplication.class, args);
+	}
+
+}
